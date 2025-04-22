b54aed1a80811560989401ef640a47f8e22d02aa
DionEmary <emarydion15@gmail.com>
2025-04-22 13:17:48 -0600

    Fixed ' causing errors

--- app/page.js
+++ app/page.js
@@ -189,7 +189,7 @@ const Home = () => {
 
         <div className="bg-[#6b7080] w-5/6 overflow-y-auto">
           <h2 className="text-gray-200 mx-6 mt-6 text-2xl font-semibold">
-            {displayName + "'s Groups"}
+            {displayName}&apos;s Groups
           </h2>
           <div className="flex flex-wrap m-4">
             {groups.length === 0 ? (
