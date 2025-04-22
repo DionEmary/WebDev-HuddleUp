863fae9c705aeb906e45ee9c1281154e6d08702d
DionEmary <emarydion15@gmail.com>
2025-04-22 14:05:43 -0600

    Fixed Inconsistent Styling

--- app/group/[groupId]/page.js
+++ app/group/[groupId]/page.js
@@ -338,7 +338,7 @@ const GroupPage = () => {
               </button>
               <button
                 onClick={handleSubmit}
-                className="bg-[#35436e] text-white px-4 py-2 rounded hover:bg-blue-600"
+                className="bg-[#35436e] text-white px-4 py-2 rounded hover:bg-[#494e5f]"
               >
                 Submit
               </button>
